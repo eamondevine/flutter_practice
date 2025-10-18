464d66951682d2be3331155178ec670b34fd3300
Eamon <eamondevine@gmail.com>
2025-10-18 16:46:57 +0800

    Setup link to firebase. Gonna need to install the stuff CLIs and stuff in other machines if I wanna use it at school

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
+import 'package:coffee_card/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:coffee_card/home.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(MaterialApp(home: Home()));
 }
 
