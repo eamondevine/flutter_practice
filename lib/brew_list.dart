008249c3a6a691219952d0281beb26c9619aedc6
eamondevine <eamondevine@gmail.com>
2025-11-03 12:24:47 +0800

    Completed vid 20, onto 21 IN this un, I figured out some stuff about outputting a ListView .builder I also reviewed what the heck these keys and superkeys are for

--- lib/brew_list.dart
+++ lib/brew_list.dart
@@ -1,7 +1,7 @@
-import 'dart:developer';
 import 'package:coffee_card/models/brew.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:coffee_card/brew_tile.dart';
 
 class BrewList extends StatefulWidget {
   const BrewList({super.key});
@@ -14,12 +14,12 @@ class _BrewListState extends State<BrewList> {
   @override
   Widget build(BuildContext context) {
     final brews = Provider.of<List<Brew>>(context);
-    for (var brew in brews) {
-      log(brew.name);
-      log(brew.sugars);
-      log(brew.strength.toString());
-    }
-    return Container();
+    return ListView.builder(
+      itemCount: brews.length,
+      itemBuilder: (context, index) {
+        return BrewTile(brew: brews[index]);
+      },
+    );
   }
 }
 
